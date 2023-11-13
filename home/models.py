a892fbb86957f493e4060a359611b2e8d5f5d907
TEST <test@TESTs-MacBook-Pro.local>
2023-11-13 11:55:50 +0000

    Added contact page and model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=20, blank=False, null=False)
+    email = models.EmailField(max_length=20, blank=False, null=False)
+    phone_number = models.CharField(max_length=15, blank=False, null=False)
+    preferred_contact_method = models.CharField(
+        max_length=20,
+        choices=[
+            ('email', 'Email'),
+            ('phone', 'Phone Call'),
+        ],
+        null=False,
+        blank=False,
+        default='Email'
+    )
+    message = models.TextField(max_length=2000, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import ContactMessage
+from .forms import ContactForm 
 
 # Create your views here.
 
@@ -6,3 +8,19 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def contact(request):
+    """ A view to handle the contact form and display the contact page """
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            success_message = "Thank you for your message! We will get in touch with you soon."
+            return render(request, 'home/contact.html', {'form': ContactForm(), 'success_message': success_message})
+    else:
+        form = ContactForm()
+
+    return render(request, 'home/contact.html', {'form': form})
