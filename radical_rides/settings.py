f4b6eb394fe601d61d334c7a968297fd4c368cac
TEST <test@TESTs-MacBook-Pro.local>
2023-11-19 18:03:13 +0000

    Removed secret key and set debug to development

--- radical_rides/settings.py
+++ radical_rides/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-75tzjxt4mbmmsf89mg-b%6ao-vdu50p@6&7nl3dy#nyx4vz#$&'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['radical-rides-7dc93d43c139.herokuapp.com','localhost','127.0.0.1']
 
