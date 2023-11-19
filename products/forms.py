42306aef8ef953d7641d17facd11c71f19f55e6b
TEST <test@TESTs-MacBook-Pro.local>
2023-11-19 12:40:51 +0000

    Added product form for admin

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import ProductReview
 
+from .models import Product, Category
+
 class ProductReviewForm(forms.ModelForm):
     class Meta:
         model = ProductReview
@@ -9,3 +11,19 @@ class ProductReviewForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['product'].widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
