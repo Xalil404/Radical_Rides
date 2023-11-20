f9c216ddd5c2622b1c0241751f7219d94ffedbda
TEST <test@TESTs-MacBook-Pro.local>
2023-11-20 12:55:08 +0000

    Upadated carousel images with new sizes and styled them for mobile

--- radical_rides/settings.py
+++ radical_rides/settings.py
@@ -22,10 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'zd1kas@x+^-+nl02*68r&2m1cr!n2*y0(u(f23k)dt4bc%8xw)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+#DEBUG = 'DEVELOPMENT' in os.environ
+
+DEBUG = True
 
 ALLOWED_HOSTS = ['radical-rides-7dc93d43c139.herokuapp.com','localhost','127.0.0.1']
 
