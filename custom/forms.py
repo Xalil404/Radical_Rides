28bee09c7689540c91b2a30a5a3ef801c8380a8f
TEST <test@TESTs-MacBook-Pro.local>
2023-11-23 12:45:20 +0000

    Styled create custom order page

--- custom/forms.py
+++ custom/forms.py
@@ -7,5 +7,11 @@ class CustomOrderForm(forms.ModelForm):
         model = CustomBoardOrder
         fields = ['board_type', 'board_class', 'board_length', 'board_width', 'board_thickness', 'image_upload', 'board_color', 'additional_notes']
         widgets = {'board_color': forms.TextInput(attrs={'type': 'color'}), 'image_upload': forms.FileInput()}
-        labels = {'image_upload': 'Upload Image'}
+        #labels = {'image_upload': 'Upload Image'}
+        labels = {
+            'board_length': 'Board Length (cm)',
+            'board_width': 'Board Width (cm)',
+            'board_thickness': 'Board Thickness (cm)',
+            'image_upload': 'Upload Image to be printed on your board'
+        }
         
