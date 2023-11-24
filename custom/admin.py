d3bd0c3ea3c826526bd5fe3a17257095ea47611e
TEST <test@TESTs-MacBook-Pro.local>
2023-11-24 14:26:27 +0000

    Customized admin panel view for custom orders list view to display order information

--- custom/admin.py
+++ custom/admin.py
@@ -3,4 +3,19 @@ from .models import CustomBoardOrder
 
 # Register your models here.
 
-admin.site.register(CustomBoardOrder)
+class CustomBoardOrderAdmin(admin.ModelAdmin):
+    list_display = ('get_username','get_email','board_type', 'board_class', 'board_length', 'board_width', 'board_thickness', 'image_upload', 'board_color', 'additional_notes')
+
+    def get_username(self, obj):
+        return obj.user.username
+    
+    def get_email(self, obj):
+        return obj.user.email
+
+    get_username.admin_order_field = 'user__username'
+    get_username.short_description = 'Username'
+
+    get_email.admin_order_field = 'user__email'
+    get_email.short_description = 'Email'
+
+admin.site.register(CustomBoardOrder, CustomBoardOrderAdmin)
