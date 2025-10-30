c389a155094fdaf623c467778075b3b56fe749e8
Xalil404 <kareemxalil@gmail.com>
2025-10-30 13:47:54 +0000

    Debug to false

--- radical_rides/settings.py
+++ radical_rides/settings.py
@@ -34,8 +34,8 @@ if not SECRET_KEY:
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
-# DEBUG = True
+# DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['radical-rides-7dc93d43c139.herokuapp.com', 'radical-rides.onrender.com', 'localhost', '127.0.0.1']
 
